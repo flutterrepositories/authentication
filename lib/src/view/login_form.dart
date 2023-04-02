075b1740d6735ea1a9ebcc22cee192ed56ac2614
Matteo Vitolo <matteo.vitolo@gmail.com>
2023-04-02 16:12:41 +0200

    corrected ids

--- lib/src/view/login_form.dart
+++ lib/src/view/login_form.dart
@@ -8,25 +8,15 @@ import 'package:flutterfire_ui/auth.dart';
 class LoginForm extends StatelessWidget {
   // Function onSignIn;
   final AuthenticationRepository _repository = AuthenticationRepository();
-  //prod
-  var webGoogleID =
-      "361359267110-4omjq2ovd4ts46tnse2oljnsu70trl5c.apps.googleusercontent.com";
-  var iosGoogleID =
-      "361359267110-n7juudgm4asfvhnb16e25jbsaotrs0mm.apps.googleusercontent.com";
-  // var iosGoogleID =
-  //     "777426915671-30sdfmtdc6qcg2la0d0m837gtsbmngo6.apps.googleusercontent.com";
-  var androidGoogleDebugID =
-      "361359267110-4omjq2ovd4ts46tnse2oljnsu70trl5c.apps.googleusercontent.com";
-  var androidGoogleID =
-      "361359267110-4omjq2ovd4ts46tnse2oljnsu70trl5c.apps.googleusercontent.com";
-  var facebookID = "158376391379190";
 
   Widget header;
-  LoginForm({required this.header});
+  String googleID;
+  String facebookID;
+  LoginForm({required this.header, required this.googleID});
 
   @override
   Widget build(BuildContext context) {
-    var gid = getGoogleID();
+    var gid = googleID;
     FlutterFireUIAuth.configureProviders([
       GoogleProviderConfiguration(clientId: gid),
       FacebookProviderConfiguration(clientId: facebookID),
@@ -48,17 +38,5 @@ class LoginForm extends StatelessWidget {
       //   );
       // },
     );
-  }
-
-  String getGoogleID() {
-    if (Platform.isIOS) {
-      return iosGoogleID;
-    } else if (Platform.isAndroid && kDebugMode) {
-      return androidGoogleDebugID;
-    } else if (Platform.isAndroid) {
-      return androidGoogleID;
-    } else {
-      return webGoogleID;
-    }
   }
 }
