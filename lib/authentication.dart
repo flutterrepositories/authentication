2500b5b8ed95fe0915759d4d44156b48a77a7c0d
Matteo Vitolo <matteo.vitolo@gmail.com>
2023-04-30 10:10:24 +0200

    added account view

--- lib/authentication.dart
+++ lib/authentication.dart
@@ -6,3 +6,4 @@ export 'src/repository/authentication_repository.dart';
 export 'src/view/login_form.dart';
 export 'src/view/logout_button.dart';
 export 'src/view/delete_user_button.dart';
+export 'src/view/account_view.dart';
